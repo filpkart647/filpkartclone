2f1a4801f5b024b9951b14c7c0d5311efcd96de5
filp <filpkartcp@gmail.com>
2025-06-06 03:24:58 +0530

    fix(socket): fixed

--- index.js
+++ index.js
@@ -68,11 +68,9 @@ io.on("connection", (socket) => {
       const message = await Message.create({ sender: senderId, receiver: receiverId, content });
       console.log({ message, connectedUsers });
   
-      const receiverSocketId = connectedUsers.get(receiverId);
-      console.log({receiverId, receiverSocketId})
-      if (receiverSocketId) {
-        io.to(receiverSocketId).emit("receiveMessage", message);
-      } 
+
+        io.emit("receiveMessage", message);
+      
     } catch (error) {
       console.error("Error in sendMessage socket event:", error);
     }
